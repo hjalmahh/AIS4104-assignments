339609e6fb1fba36d1b1db5123f12e3b3b969460
hjalmahh <hjalmahh@stud.ntnu.no>
2024-08-23 11:59:42 +0200

    First lesson thingtest

--- assignment-1/main.cpp
+++ assignment-1/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
 #include <Eigen/Dense>
+#include <math.h>
+
+double deg_to_rad(double degrees)
+
+{
+    return degrees * M_PI/180;
+}
+double rad_to_deg(double radians)
+{
+    return radians * 180 / M_PI;
+}
+
+
+
+Eigen::Matrix3d rotate_x(double angle)
+{
+    Eigen::Matrix3d matrix;
+    matrix <<
+        1.0, 0.0, 0.0,
+        0.0, std::cos(angle), -std::sin(angle),
+        0.0, std::sin(angle), std::cos(angle);
+    return matrix;
+}
+
 
 void example(double constant)
 {
@@ -15,6 +39,8 @@ void example(double constant)
 
 int main()
 {
-    example(2.0);
+    //example(2.0);
+
+
     return 0;
 }
